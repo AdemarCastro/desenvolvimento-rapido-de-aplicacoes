8e3795dba8a85bb0d9fb50e5eca122147662a0d3
AdemarCastro <2022002444@ifam.edu.br>
2024-10-17 22:32:05 -0400

    [FEAT] - Adiciona a implementação dos métodos persistir, listar e consultar na classe TestaPessoa

--- src/main/java/modelo/Pessoa.java
+++ src/main/java/modelo/Pessoa.java
@@ -1,40 +1,114 @@
 package modelo;
-import javax.persistence.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * A classe Pessoa representa uma entidade de pessoa com atributos nome, telefone e email.
+ *
+ * Anotada com @Entity, esta classe pode ser mapeada para uma tabela em um banco de dados.
+ *
+ * A classe inclui construtores, getters e setters para seus atributos.
+ */
 @Entity
 public class Pessoa {
+
+    // Atributos da entidade Pessoa
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
 
+    /**
+     * Construtor que inicializa uma nova instância de Pessoa com nome, telefone e email.
+     *
+     * @param nome o nome da pessoa
+     * @param telefone o telefone da pessoa
+     * @param email o email da pessoa
+     */
     public Pessoa(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
     }
 
+    /**
+     * Construtor padrão.
+     */
     public Pessoa() {}
 
+    /**
+     * Obtém o ID da pessoa.
+     *
+     * @return o ID da pessoa
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Define o ID da pessoa.
+     *
+     * @param id o ID a ser definido
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Obtém o nome da pessoa.
+     *
+     * @return o nome da pessoa
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome da pessoa.
+     *
+     * @param nome o nome a ser definido
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Obtém o telefone da pessoa.
+     *
+     * @return o telefone da pessoa
+     */
     public String getTelefone() {
         return telefone;
     }
 
+    /**
+     * Define o telefone da pessoa.
+     *
+     * @param telefone o telefone a ser definido
+     */
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
+    /**
+     * Obtém o email da pessoa.
+     *
+     * @return o email da pessoa
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Define o email da pessoa.
+     *
+     * @param email o email a ser definido
+     */
     public void setEmail(String email) {
         this.email = email;
     }
--- src/main/java/teste/TestaPessoa.java
+++ src/main/java/teste/TestaPessoa.java
@@ -1,20 +1,85 @@
 package teste;
 
 import modelo.Pessoa;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
 
+/**
+ * A classe TestaPessoa demonstra o uso básico da JPA (Java Persistence API) para
+ * persistir uma entidade Pessoa no banco de dados.
+ *
+ * Esta classe cria uma instância de Pessoa, inicia uma transação, persiste a
+ * entidade no banco de dados e, em seguida, faz o commit da transação.
+ *
+ * O nome da unidade de persistência "Banco01PU" deve estar definido no arquivo `persistence.xml`.
+ */
 public class TestaPessoa {
-    public static void main(String[] args) {
-       EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Banco01PU");
+
+    private static void persistir(){
+        // Cria a EntityManagerFactory usando o nome da unidade de persistência "Banco01PU"
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("SistemaPU");
+
+        // Cria o EntityManager
         EntityManager entityManager = fabrica.createEntityManager();
-        Pessoa pessoa = new Pessoa("Rogerio", "3234-3543", "rogerio@email.com");
+
+        Pessoa pessoa = new Pessoa("Rogerio2", "3234-5678", "rogerio@email.com");
+
         entityManager.getTransaction().begin();
+
         entityManager.persist(pessoa);
+
         entityManager.getTransaction().commit();
+
         entityManager.close();
+
+        fabrica.close();
+
+    }
+
+    private static void listar(){
+        // Cria a EntityManagerFactory usando o nome da unidade de persistência "Banco01PU"
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("SistemaPU");
+
+        // Cria o EntityManager
+        EntityManager entityManager = fabrica.createEntityManager();
+
+        String consulta = "select p from Pessoa p";
+
+        Query query = entityManager.createQuery(consulta);
+
+        List<Pessoa> pessoas = query.getResultList();
+
+        for(Pessoa p: pessoas){
+            System.out.println(p.getNome());
+            System.out.println(p.getEmail());
+            System.out.println(p.getTelefone());
+            System.out.println("***************");
+        }
+    }
+
+    private static void consultar(){
+        // Cria a EntityManagerFactory usando o nome da unidade de persistência "Banco01PU"
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("SistemaPU");
+
+        // Cria o EntityManager
+        EntityManager entityManager = fabrica.createEntityManager();
+
+        Pessoa pessoa = entityManager.find(Pessoa.class, 1l);
+
+        System.out.println("Pessoa:" + pessoa.getNome());
+
+        entityManager.close();
+
         fabrica.close();
     }
+
+    public static void main(String[] args) {
+//        persistir();
+//        listar();
+        consultar();
+    }
+
 }
